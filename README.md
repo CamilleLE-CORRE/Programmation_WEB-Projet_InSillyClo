1b716b4148550c69564d8b1b2a7e7f32903c316c
muyao.guo <muyao.guo@etu-upsaclay.fr>
2026-01-06 22:26:27 +0100

    1. requirements.txt: needed packages and interpreter 2. Added command in "README.md" to install requirements. 3. correspondences apps construction

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Membres du projet :
 Commandes à exécuter :   
 
 ```
+pip install -r requirements.txt
 git clone <[url_du_repo](https://github.com/CamilleLE-CORRE/Programmation_WEB-Projet_InSillyClo)>  
 cd Programmation_WEB-Projet_InSillyClo  
 python manage.py makemigrations  
--- apps/correspondences/admin.py
+++ apps/correspondences/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Correspondence, CorrespondenceEntry
 
-# Register your models here.
+@admin.register(Correspondence)
+class CorrespondenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'is_public')
+    search_fields = ('name', 'owner__email')
+
+
+@admin.register(CorrespondenceEntry)
+class CorrespondenceEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'correspondence', 'identifier', 'display_name', 'entry_type')
+    search_fields = ('identifier', 'display_name', 'correspondence__name')
+    list_filter = ('entry_type','correspondence')
