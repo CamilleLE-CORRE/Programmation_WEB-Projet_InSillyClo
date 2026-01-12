0476f4bc76381ab881fe3bebc2f21a87ff707af5
CamilleLE-CORRE <camille.le-corre@etu-upsaclay.fr>
2026-01-12 10:25:12 +0100

    Table for plasmid list

--- apps/plasmids/urls.py
+++ apps/plasmids/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PlasmidSearchView
-from .views import PlasmidList
+from .views import PlasmidSearchView, plasmid_list, plasmid_detail
 
 app_name = "plasmids"
 urlpatterns = [
+    path("plasmid_list/", plasmid_list, name="plasmid_list"),
+    path("<str:identifier>/", plasmid_detail, name="plasmid_detail"),
     path("search/", PlasmidSearchView.as_view(), name="search"),
-    path("plasmid_list/", PlasmidList.as_view(), name="plasmid_list"),
 ]
--- apps/plasmids/views.py
+++ apps/plasmids/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from .models import Plasmid, PlasmidCollection
 
@@ -6,20 +6,28 @@ from .models import Plasmid, PlasmidCollection
 class PlasmidSearchView(TemplateView):
     template_name = "plasmids/search.html"
 
-class PlasmidList(TemplateView):
-    template_name = "plasmids/plasmid_list.html"
+# class PlasmidList(TemplateView):
+#     template_name = "plasmids/plasmid_list.html"
 
 def plasmid_list(request):
+
+    # A connected user can see both public and private plasmid collections
     if request.user.is_authenticated:
-        plasmids = Plasmid.objects.filter(
-            collection__is_public=True
-        ) | Plasmid.objects.filter(
-            collection__owner=request.user
-        )
+        plasmids = Plasmid.objects.all()
+
+    # An unregistered user can only see public plasmid collections
     else:
         plasmids = Plasmid.objects.filter(collection__is_public=True)
+
+    return render(request, 'plasmids/plasmid_list.html', {
+        'plasmids': plasmids
+    })
+
+def plasmid_detail(request, identifier):
     
+    # Get plasmid and verify it exists
+    plasmid = get_object_or_404(Plasmid, identifier=identifier)
     
-    return render(request, 'plasmids/list.html', {
-        'plasmids': plasmids.distinct()
+    return render(request, "plasmids/plasmid_detail.html", {
+        "plasmid": plasmid
     })
