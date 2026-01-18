70862cc56ac02c1fdce8b5e0cae575b0dc0050b2
muyao.guo <muyao.guo@etu-upsaclay.fr>
2026-01-18 14:27:30 +0100

    render "correspondence table" and "correspondence entries"

--- apps/correspondences/urls.py
+++ apps/correspondences/urls.py
@@ -6,6 +6,6 @@ app_name = "correspondences"
 urlpatterns = [
     path("", views.correspondence_list, name="list"),
     path("<int:pk>/", views.correspondence_detail, name="detail"),
-    path("<int:pk>/upload", views.correspondence_upload, name="upload"),
-    path("new", views.correspondence_create, name="create"),
+    path("<int:pk>/upload/", views.correspondence_upload, name="upload"),
+    path("new/", views.correspondence_create, name="create"),
 ]
