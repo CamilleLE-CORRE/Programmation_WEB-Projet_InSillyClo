25262c2e5789ccaaf9331fc2f1058867394bc14c
CamilleLE-CORRE <camille.le-corre@etu-upsaclay.fr>
2026-01-28 10:14:44 +0100

    Updating templates with form or file + uniformisation des forms

--- apps/campaigns/urls.py
+++ apps/campaigns/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('download/<int:template_id>/', views.download_template, name='download_template'),
     path('public/<int:template_id>/download/', views.download_public_template, name='download_public_template'),
     path('templates/<int:template_id>/delete/', views.delete_template, name='delete_template'),
+    path('templates/<int:template_id>/edit/', views.edit_template, name='edit_template'),
+    path('templates/<int:template_id>/replace-xlsx/', views.replace_template_xlsx, name='replace_template_xlsx'),
 ]
 
