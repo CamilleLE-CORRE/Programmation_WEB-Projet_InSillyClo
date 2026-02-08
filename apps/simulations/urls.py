d245c460bdfe82407ae98495439206a0b26069df
Laura LEFEVRE <laura.lefevre@ens.uvsq.fr>
2026-02-08 13:43:24 +0100

    Ajout fonctionnalité suppression historique + Nettoyage

--- apps/simulations/urls.py
+++ apps/simulations/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.simulation_view, name='simu'),
     path('history/', views.simulation_history_view, name='history'),
     path('results/<str:sim_id>/', views.simulation_detail_view, name='simulation_detail'),
+    path('delete/', views.delete_campaigns_view, name='delete_campaigns'),
 ]
